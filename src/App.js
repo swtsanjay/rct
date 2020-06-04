33ae125ca638ca0f9e52a03c1a60d23bcd835d21
Sanjay <sanjayyadav19298@gmail.com>
2020-06-04 10:46:28 +0530

    no message

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h1>Hello</h1>
+                <Person></Person>
+                <Person></Person>
+                <Person></Person>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const person = () =>{
+    return <h1>Mai hun Don</h1>
+}
+
+export default person;
