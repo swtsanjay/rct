579adee1ca1566719c88b040e35ea5e382c42fdc
satpnm <sat.web1989@gmail.com>
2020-06-05 11:50:26 +0530

    no message

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Person from './Person/Person';
+import Person from './Person/Person'
+import person from './Person/Person';
 
 class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <h1>Hello</h1>
-                <Person></Person>
-                <Person></Person>
-                <Person></Person>
-            </div>
-        );
+    state = {
+        person: [
+            {name: 'Sahil', age:22},
+            {name: 'Bittu', age:20},
+            {name: 'Mukesh', age:21}
+        ]
     }
+
+    clk = () =>{
+        console.log("clicked");
+    }
+    render() {
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+            <button onClick={this.clk()}>Click Me</button>
+          {/* <Person  name={this.state.person[0].name} age={this.state.person[0].age}/> */}
+          <Person  name={this.state.person[1].name} age={this.state.person[1].age}>My hobbies: watching movie</Person>
+          {/* <Person  name={this.state.person[2].name} age={this.state.person[2].age}/> */}
+        </p>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,7 @@
 import React from "react"
 
-const person = () =>{
-    return <h1>Mai hun Don</h1>
+const person = (p) =>{
+    return <p>I am {p.name} and {p.age} years old. <b>{p.children}</b></p>
 }
 
 export default person;
